04ad8c844cf57b73b8d911cc392a96fb35e69241
Miessok Eric Cedric Junior <miessokeric@gmail.com>
2024-05-05 17:08:13 +0100

    User API

--- app/user/urls.py
+++ app/user/urls.py
@@ -9,5 +9,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenVIew.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me')
 ]
